1cfd2f00b7d414107731cbbe6a7f2e10bb98de79
arshkharbanda2010 <arsh.kharbanda2010@gmail.com>
2021-09-15 17:28:43 +0530

    rouing done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from './pages';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React from "react";
+import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from "./pages";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Dashboard></Dashboard>
-      <Login />
-      <Error />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact={true} path="/">
+          <Dashboard></Dashboard>
+        </Route>
+        <Route exact={true} path="/login">
+          <Login />
+        </Route>
+        <Route exact={true} path="*">
+          <Error></Error>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,8 +1,20 @@
-import React from 'react';
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 const Error = () => {
-  return <h2>error page</h2>;
+  return (
+    <Wrapper>
+      <div>
+        <h2>404 not found</h2>
+        <h4>Please navigate to home</h4>
+      </div>
+      <div>
+        <Link to="/" className="btn">
+          Back home
+        </Link>
+      </div>
+    </Wrapper>
+  );
 };
 const Wrapper = styled.section`
   min-height: 100vh;
